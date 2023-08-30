08837f1400ff91dfd64d87838d3c7ecd267d0272
Dnyaneshwar <dnyaneshwarpatil830831@gmail.com>
2023-08-30 17:43:52 +0530

    new portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nilesh7007.github.io
+This is my Portfolio.
+https://Nilesh7007.github.io
---
+++ index.js
@@ -0,0 +1,23 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document.getElementById('resume-button-1').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1kxNDNej5oLHUwG-obJzqBXyn6iHCNXWT/view?usp=sharing", "_blank");
+});
+
+document.getElementById('resume-button-2').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1kxNDNej5oLHUwG-obJzqBXyn6iHCNXWT/view?usp=sharing", "_blank");
+});
+
