74f7451b2941253f8b3327c2eefbb4e1ab2e617c
Dnyaneshwar <dnyaneshwarpatil830831@gmail.com>
2023-12-06 13:23:26 +0530

    new resume

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ window.onscroll = () => {
 };
 
 document.getElementById('resume-button-1').addEventListener("click", () => {
-  window.open("https://drive.google.com/file/d/1kxNDNej5oLHUwG-obJzqBXyn6iHCNXWT/view?usp=sharing", "_blank");
+  window.open("https://drive.google.com/file/d/1AhSS8JwlkxcRRzq7_Vwwnid5med0ZZD5/view?usp=drive_link", "_blank");
 });
 
 document.getElementById('resume-button-2').addEventListener("click", () => {
-  window.open("https://drive.google.com/file/d/1kxNDNej5oLHUwG-obJzqBXyn6iHCNXWT/view?usp=sharing", "_blank");
+  window.open("https://drive.google.com/file/d/1AhSS8JwlkxcRRzq7_Vwwnid5med0ZZD5/view?usp=drive_link", "_blank");
 });
 
